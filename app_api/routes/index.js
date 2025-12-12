88f333d17afd79afd0b14f0e3a29e87817627d0f
anilelmz <elmaz895@gmail.com>
2025-12-12 17:57:39 +0300

    cmt

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,27 +1,26 @@
-var express = require('express');
-var router = express.Router();
-var venuController = require("../controller/VenueController");
-var commentController = require("../controller/CommentController");
-
+var express= require('express');
+var router=express.Router();
+var venueController=require("../controller/VenueController");
+var commentController=require("../controller/CommentController");
 router
 .route("/venues")
-.get(venuController.listVenues)
-.post(venuController.addVenue);
+.get(venueController.listVenues)
+.post(venueController.addVenue);
 
 router
 .route("/venues/:venueid")
-.get(venuController.getVenue)
-.put(venuController.updateVenue)
-.delete(venuController.deleteVenue);
+.get(venueController.getVenue)
+.put(venueController.updateVenue)
+.delete(venueController.deleteVenue);
 
 router
 .route("/venues/:venueid/comments")
 .post(commentController.addComment)
 
-router 
+router
 .route("/venues/:venueid/comments/:commentid")
 .get(commentController.getComment)
 .put(commentController.updateComment)
 .delete(commentController.deleteComment);
 
-module.exports = router;
+module.exports=router;
